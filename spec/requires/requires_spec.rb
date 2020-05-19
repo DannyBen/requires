5521172ce8e8c0195ea824540a6a40ffbebb4e9b
Danny Ben Shitrit <db@dannyben.com>
2020-05-19 18:57:39 +0000

    migrate from rspec_fixtures to rspec_approvals

--- spec/requires/requires_spec.rb
+++ spec/requires/requires_spec.rb
@@ -3,25 +3,25 @@ require 'spec_helper'
 describe 'requires' do
   context "without arguments" do
     it "requires current directory" do
-      expect(runner 'without_arguments').to match_fixture "without_arguments"
+      expect(runner 'without_arguments').to match_approval "without_arguments"
     end
   end
 
   context "with a file with extension" do
     it "requires it" do
-      expect(runner 'file_with_extension').to match_fixture "file_with_extension"
+      expect(runner 'file_with_extension').to match_approval "file_with_extension"
     end
   end
 
   context "with a file without extension" do
     it "requires it" do
-      expect(runner 'file_without_extension').to match_fixture "file_without_extension"
+      expect(runner 'file_without_extension').to match_approval "file_without_extension"
     end
   end
 
   context "with a library name" do
     it "requires it" do
-      expect(runner 'library').to match_fixture "library"
+      expect(runner 'library').to match_approval "library"
     end
   end
 
@@ -33,19 +33,19 @@ describe 'requires' do
 
   context "with multiple items" do
     it "requires everything" do
-      expect(runner 'multiple_items').to match_fixture "multiple_items"
+      expect(runner 'multiple_items').to match_approval "multiple_items"
     end
   end
 
   context "with a glob pattern that does not end with .rb" do
     it "requires all matching files" do
-      expect(runner 'glob_without_extension').to match_fixture "glob_without_extension"
+      expect(runner 'glob_without_extension').to match_approval "glob_without_extension"
     end
   end
 
   context "with a glob pattern that does ends with .rb" do
     it "requires all matching files" do
-      expect(runner 'glob_with_extension').to match_fixture "glob_with_extension"
+      expect(runner 'glob_with_extension').to match_approval "glob_with_extension"
     end
   end
 end
